367f6500b02a5d1ac28e9c9260544940faea2673
VitorMassita <vitor.mz@puccampinas.edu.br>
2026-03-24 16:14:15 -0300

    trabalhos dia 24/03/2026

---
+++ menu.py
@@ -0,0 +1,18 @@
+import gerenciamento
+def iniciar():
+    print("Abrindo gerenciamento")
+while True:
+    try:
+        print("\n1 - Gerenciamento \n2 - Votação")
+        escolha = int(input("Escolha a opção desejada: "))
+        if escolha == 1:
+            print("Gerenciamento")
+            gerenciamento.iniciar()
+            break    
+        elif escolha == 2:
+            print("Votação")
+            break
+        else:
+            print("Opção inválida, tente novamente.")
+    except ValueError:
+        print("Entrada inválida. Digite um número.")
