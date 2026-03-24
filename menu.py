5bb1be33d464f2a5174039e64981aac21f453421
Lucas <lucasminneh270508@gmail.com>
2026-03-24 16:16:49 -0300

    arrumando-Trabalho-24/03

--- menu.py
+++ menu.py
@@ -1,13 +1,9 @@
-import gerenciamento
-def iniciar():
-    print("Abrindo gerenciamento")
 while True:
     try:
         print("\n1 - Gerenciamento \n2 - Votação")
         escolha = int(input("Escolha a opção desejada: "))
         if escolha == 1:
             print("Gerenciamento")
-            gerenciamento.iniciar()
             break    
         elif escolha == 2:
             print("Votação")
